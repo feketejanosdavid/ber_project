c587a2d095d6fbc0e5d017b4da8a9c60e2cabc4e
valaki <valaki@gmail.com>
2023-12-20 10:03:17 +0100

    majdnem kész

---
+++ dolgozo.py
@@ -0,0 +1,9 @@
+#Tároló osztály
+#Egy dolgozó tulajdonságainak tárolásáram hoztuk létre
+class Dolgozo:
+    def __init__(self, nev, telepules, cim, szuletes, fizetes):
+        self.nev = nev
+        self.telepules = telepules
+        self.cim = cim
+        self.szuletes = szuletes
+        self.fizetes = fizetes
---
+++ main.py
@@ -0,0 +1,35 @@
+import math
+from dolgozo import Dolgozo
+
+class Ber:
+    #Konstruktor - az osztályváltozóknak kezdő értéket adunk
+    def __init__(self):
+        self.filename = 'berkft.txt'
+        self.dolgozo_lista = []
+
+    #Fájl beolvasása
+    def olvas_fajl(self):
+        fp = open(self.filename, 'r', encoding='UTF-8')
+        lines = fp.readlines()
+        fp.close()
+        for line in lines:
+            line = line.rstrip()
+            (nev, telepules, cim, szuletes, fizetes) = line.split(':')
+            dolgozo = Dolgozo(nev, telepules, cim, szuletes, fizetes)
+            self.dolgozo_lista.append(dolgozo)
+
+    #Szolnokiak
+    def szolnoki(self):
+        szolnoki_lista = []
+        for dolgozo in self.dolgozo_lista:
+            if dolgozo.telepules == 'Szolnok':
+                szolnoki_lista.append(dolgozo)
+        max_szolnoki = szolnoki_lista[0]
+        for szolnoki in szolnoki_lista:
+            if szolnoki.szuletes > max_szolnoki.szuletes:
+                max_szolnoki = szolnoki
+        print(max_szolnoki.szuletes)
+
+ber = Ber()
+ber.olvas_fajl()
+ber.szolnoki()
